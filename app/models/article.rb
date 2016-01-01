4ce18ebfb60b3cf0cb0a54c58d17bef00c659dd9
Olyad Regassa <olyad33@yahoo.com>
2016-01-01 03:00:26 -0600

    Install bootstrap

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ActiveRecord::Base
 
 validates :title, presence: true, length: {minimum: 3, maximum: 50}
-validates :description, presence: true, length: {minimum: 10, maximum: 300}
-
+              
+#validate the title of it's precence is true 
+#and length is min of 3 and max 50.
+validates :description, presence: true, length: {minimum: 10, maximum: 300}                  
 end 
+
