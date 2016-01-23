a110ac96d6191cc8a881f37504ceb75b48676bf6
Olyad Regassa <olyad33@yahoo.com>
2016-01-22 18:05:01 -0600

    Sign up users from the ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
- 
+  resources :articles
   root 'pages#home'
   get 'about', to: 'pages#about'
-
-  resources :articles
-  
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
 end
